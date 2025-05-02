5df37d2bb92836abc7ba7f3282ff89d87c68c386
larpsuli <95588579+larpsuli@users.noreply.github.com>
2025-05-02 08:10:09 +0300

    Create Tietokantayhteys.java

---
+++ src/util/Tietokantayhteys.java
@@ -0,0 +1,15 @@
+package util;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Tietokantayhteys {
+    private static final String URL = "jdbc:mysql://localhost:3306/mokkimanagerpro";
+    private static final String USER = "root"; //tähän oma käyttäjänimi
+    private static final String PASSWORD = "root1234"; //tähän oma salasana
+
+    public static Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(URL, USER, PASSWORD);
+    }
+}

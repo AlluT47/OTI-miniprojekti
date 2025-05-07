68f09666442cd3dedb6f45ef55dac8c682a2dac6
larpsuli <95588579+larpsuli@users.noreply.github.com>
2025-05-07 10:30:13 +0300

    Luotu malliluokka varaukselle

---
+++ src/main/java/model/Varaus.java
@@ -0,0 +1,67 @@
+package model;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+public class Varaus {
+    private int varausId; // Varauksen id
+    private int asiakasId; // Asiakkaan id
+    private int mokkiId; // Mökin id
+    private LocalDate varauksenAlku; // Varauksen alkupvm
+    private LocalDate varauksenLoppu; // Varauksen loppupvm
+    private String tila;
+    private LocalDateTime varausAika; 
+
+    public Varaus(int varausId, int asiakasId, int mokkiId, LocalDate varauksenAlku, LocalDate varauksenLoppu, String tila, LocalDateTime varausAika) {
+        this.varausId = varausId;
+        this.asiakasId = asiakasId;
+        this.mokkiId = mokkiId;
+        this.varauksenAlku = varauksenAlku;
+        this.varauksenLoppu = varauksenLoppu;
+        this.tila = tila;
+        this.varausAika = varausAika;
+    }
+
+    // Getterit
+    public int getVarausId(){
+        return varausId;
+    }
+    public int getAsiakasId(){
+        return asiakasId;
+    }
+    public int getMokkiId(){
+        return mokkiId;
+    }
+    public LocalDate getVarauksenAlku(){
+        return varauksenAlku;
+    }
+    public LocalDate getVarauksenLoppu(){
+        return varauksenLoppu;
+    }
+    public String getTila(){
+        return tila;
+    }
+    public LocalDateTime getVarausAika(){
+        return varausAika;
+    }
+
+    // Setterit
+    public void setAsiakasId(int asiakasId){
+        this.asiakasId = asiakasId;
+    }
+    public void setMokkiId(int mokkiId){
+        this.mokkiId = mokkiId;
+    }
+    public void setVarauksenAlku(LocalDate varauksenAlku){
+        this.varauksenAlku = varauksenAlku;
+    }
+    public void setVarauksenLoppu(LocalDate varauksenLoppu){
+        this.varauksenLoppu = varauksenLoppu;
+    }
+    public void setTila(String tila){
+        this.tila = tila;
+    }
+    public void setVarausAika(LocalDateTime varausAika){
+        this.varausAika = varausAika;
+    }
+}

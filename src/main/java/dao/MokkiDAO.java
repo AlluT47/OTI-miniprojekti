a89cec1b7000e8cb140857d1e55e45090b323d20
larpsuli <95588579+larpsuli@users.noreply.github.com>
2025-05-07 08:42:02 +0300

    Lisätty metodit mökin lisäämiselle, muokkaamiselle ja poistamiselle

--- src/main/java/dao/MokkiDAO.java
+++ src/main/java/dao/MokkiDAO.java
@@ -1,6 +1,7 @@
 package dao;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
@@ -11,6 +12,7 @@ import model.Mokki;
 import util.Tietokantayhteys;
 
 public class MokkiDAO {
+
     public List<Mokki> haeKaikkiMokit() {
         List<Mokki> mokit = new ArrayList<>();
         String sql = "SELECT * FROM mokki";
@@ -37,6 +39,62 @@ public class MokkiDAO {
         }
 
         return mokit;
+    }
+
+    // Lisää uuden mökin tietokantaan
+    public void lisaaMokki(Mokki mokki) {
+        String sql = "INSERT INTO mokki (nimi, osoite, kuvaus, hinta_per_yö, kapasiteetti, on_vapaana) VALUES (?, ?, ?, ?, ?, ?)";
+
+        try (Connection conn = Tietokantayhteys.getConnection();
+            PreparedStatement pstmt = conn.prepareStatement(sql)) {
+
+            pstmt.setString(1, mokki.getNimi());
+            pstmt.setString(2, mokki.getOsoite());
+            pstmt.setString(3, mokki.getKuvaus());
+            pstmt.setDouble(4, mokki.getHintaPerYö());
+            pstmt.setInt(5, mokki.getKapasiteetti());
+            pstmt.setBoolean(6, mokki.isOnVapaana());
+            pstmt.executeUpdate();
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    // Päivittää mökin tiedot tietokantaan
+    public void paivitaMokki(Mokki mokki) {
+        String sql = "UPDATE mokki SET nimi = ?, osoite = ?, kuvaus = ?, hinta_per_yö = ?, kapasiteetti = ?, on_vapaana = ? WHERE id = ?";
+
+        try (Connection conn = Tietokantayhteys.getConnection();
+             PreparedStatement pstmt = conn.prepareStatement(sql)) {
+
+            pstmt.setString(1, mokki.getNimi());
+            pstmt.setString(2, mokki.getOsoite());
+            pstmt.setString(3, mokki.getKuvaus());
+            pstmt.setDouble(4, mokki.getHintaPerYö());
+            pstmt.setInt(5, mokki.getKapasiteetti());
+            pstmt.setBoolean(6, mokki.isOnVapaana());
+            pstmt.setInt(7, mokki.getMokkiId());
+            pstmt.executeUpdate();
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    // Poistaa mökin tietokannasta id:n perusteella
+    public void poistaMokki(int mokkiId) {
+        String sql = "DELETE FROM mokki WHERE id = ?";
+
+        try (Connection conn = Tietokantayhteys.getConnection();
+             PreparedStatement pstmt = conn.prepareStatement(sql)) {
+
+            pstmt.setInt(1, mokkiId);
+            pstmt.executeUpdate();
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
     
 }
